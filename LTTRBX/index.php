d43c0eb17b0d9e47398603323cf03529c18ff7a3
mehul <rathodmehul782@gmail.com>
2023-04-08 18:11:29 +0530

    commit

---
+++ LTTRBX/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<!--css-->
+	<link rel="stylesheet" type="text/css" href="assets/plugin/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.css">
+</head>
+<body>
+	
+
+
+	<!--js-->
+	<script src="assets/plugin/jquery/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/javascript.js"></script>
+</body>
+</html>
