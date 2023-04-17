2aff1d9fa1007d59b3a8f80e2d95c439ccbd4ddd
mehul <rathodmehul782@gmail.com>
2023-04-17 12:36:17 +0530

    register page
    set background image

--- LTTRBX/header.php
+++ LTTRBX/header.php
@@ -3,11 +3,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
-
 	<!--css-->
 	<link rel="stylesheet" type="text/css" href="../assets/plugin/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 	<link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../assets/image"> -->
 	<link rel="stylesheet" type="text/css" href="../assets/fontawesome/css/all.css">
 </head>
-<body>
+<body class="back-img">
