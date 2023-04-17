b0e6a744eabe19baca31ce76f44e8af6398565b2
mehul <rathodmehul782@gmail.com>
2023-04-17 11:42:35 +0530

    commit

--- LTTRBX/Dashboard/template/pages/header_info/header_info.php
+++ LTTRBX/Dashboard/template/pages/header_info/header_info.php
@@ -270,6 +270,7 @@
             </a>
             <div class="collapse" id="setting">
               <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="../header_info/header_info.php">Header Info</a></li>
                 <li class="nav-item"> <a class="nav-link" href="../header/header_menu.php">Header</a></li>
                 <li class="nav-item"> <a class="nav-link" href="../image/image_upload.php">image</a></li>
                 <li class="nav-item"> <a class="nav-link" href="../slider/slider.php">Slider</a></li>
